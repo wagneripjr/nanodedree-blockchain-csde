905dac5094ac4d9e1dd31edbe54bd7f19f5e6c74
Wagner Ignacio Pinto Junior <wagneripjr@adustio.com.br>
2020-02-21 09:24:22 -0400

    Using Ganache Gui as test network because truffle dev was throwing "[ethjs-rpc] rpc error with payload"

--- truffle-config.js
+++ truffle-config.js
@@ -46,9 +46,9 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    develop: {
+    developer: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*"
     },
 
